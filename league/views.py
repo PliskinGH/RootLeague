9552b9003f7f751b8b95da37f2eea378d3e8aab5
Pliskin <nabil14@gmail.com>
2024-08-05 19:56:36 +0200

    leaderboard relative score

--- league/views.py
+++ league/views.py
@@ -1,4 +1,4 @@
-from django.db.models import Sum
+from django.db.models import Sum, Count, Q, F
 from django.utils.translation import gettext_lazy as _
 
 from authentification.models import Player
@@ -7,13 +7,16 @@ from matchmaking.views import ElidedListView
 # Create your views here.
 
 def leaderboard(request, players = None, title = _("All players"),
-            number_per_page = 10):
+                ordering = '-relative_score', number_per_page = 15):
     if (players is None):
-        players = Player.objects.annotate(score=Sum('participations__tournament_score')).exclude(score=None)
+        players = Player.objects.all()
+    players = players.annotate(total=Count('participations')).exclude(Q(total=None) | Q(total__lt=1)) \
+                     .annotate(score=Sum('participations__tournament_score')).exclude(score=None) \
+                     .annotate(relative_score=F('score')/F('total')*100)
     return ElidedListView.as_view(model=Player,
                                   queryset=players,
                                   template_name='league/leaderboard.html',
                                   paginate_by=number_per_page,
                                   title=title,
-                                  ordering="-score"
+                                  ordering=ordering
                                   )(request)
