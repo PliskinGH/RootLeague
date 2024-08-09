a8d1bdac2707a7eda621c569342b7e617d74d062
Pliskin <nabil14@gmail.com>
2024-08-09 23:26:24 +0200

    faction stats

--- league/urls.py
+++ league/urls.py
@@ -9,5 +9,10 @@ urlpatterns = [
     path('<int:league_id>/', views.league_leaderboard, name='league_leaderboard'),
     path('tournament/', views.tournament_leaderboard, name='default_tournament_leaderboard'),
     path('tournament/<int:tournament_id>/', views.tournament_leaderboard, name='tournament_leaderboard'),
-    path('global', views.global_leaderboard, name='global_leaderboard'),
+    path('global/', views.leaderboard, name='global_leaderboard'),
+    path('factions/', views.league_faction_stats, name='default_faction_stats'),
+    path('factions/<int:league_id>/', views.league_faction_stats, name='league_faction_stats'),
+    path('factions/tournament/', views.tournament_faction_stats, name='default_tournament_faction_stats'),
+    path('factions/tournament/<int:tournament_id>/', views.tournament_faction_stats, name='tournament_faction_stats'),
+    path('factions/global/', views.faction_stats, name='global_faction_stats'),
 ]
--- league/views.py
+++ league/views.py
@@ -2,10 +2,13 @@ from django.db.models import Sum, Count, Q, F
 from django.utils.translation import gettext_lazy as _
 from django.core.validators import EMPTY_VALUES
 from django.core.paginator import Paginator
+from django.views.generic import TemplateView
 
 from .models import League, Tournament
 from authentification.models import Player
+from matchmaking.models import Participant
 from misc.views import ElidedListView
+from .constants import FACTIONS
 
 # Create your views here.
 
@@ -41,13 +44,121 @@ def leaderboard(request,
     players = players.annotate(relative_score=F('score')/F('total')*100)
 
     if (title in EMPTY_VALUES):
-        if (tournament not in EMPTY_VALUES):
-            title = tournament.name
-        elif (league not in EMPTY_VALUES):
-            title = league.name
-        else:
-            title = _("All players")
+        title = get_title(tournament=tournament,
+                          league=league)
     
+    extra_context = get_menu_by_pagination(tournament=tournament,
+                                           league=league)
+    extra_context['min_games'] = min_games
+    
+    return ElidedListView.as_view(model=Player,
+                                  queryset=players,
+                                  template_name='league/leaderboard.html',
+                                  paginate_by=number_per_page,
+                                  title=title,
+                                  ordering=ordering,
+                                  extra_context=extra_context
+                                  )(request)
+
+def league_leaderboard(request,
+                       league_id = None,
+                       ordering = '-relative_score',
+                       number_per_page = 15):
+    league = get_league(league_id)
+    return leaderboard(request, league=league,
+                       ordering=ordering,
+                       number_per_page=number_per_page)
+
+def tournament_leaderboard(request,
+                           tournament_id = None,
+                           ordering = '-relative_score',
+                           number_per_page = 15):
+    tournament = get_tournament(tournament_id)
+    return leaderboard(request, tournament=tournament,
+                       ordering=ordering,
+                       number_per_page=number_per_page)
+
+def faction_stats(request,
+                  league = None,
+                  tournament = None,
+                  title = None):
+    stats = []
+    for (faction, faction_name) in FACTIONS:
+        participations = Participant.objects.filter(faction=faction)
+        if (tournament not in EMPTY_VALUES):
+            participations = participations.filter(match__tournament=tournament)
+        elif (league not in EMPTY_VALUES):
+            participations = participations.filter(match__tournament__league=league)
+        total = participations.count()
+        if (total < 1):
+            faction_stats = dict(total=total,
+                                 score=None,
+                                 relative_score=None)
+        else:
+            faction_stats = participations.aggregate(score=Sum('tournament_score'))
+            faction_stats['total'] = total
+            faction_stats['relative_score'] = faction_stats['score'] / total * 100
+        faction_stats['name'] = faction_name
+        stats.append(faction_stats)
+
+    if (title in EMPTY_VALUES):
+        title = get_title(tournament=tournament,
+                          league=league)
+    
+    extra_context = get_menu_by_pagination(tournament=tournament,
+                                           league=league)
+    extra_context['stats'] = stats
+    extra_context['title'] = title
+
+    return TemplateView.as_view(template_name='league/faction_stats.html',
+                                extra_context=extra_context
+                                )(request)
+
+def tournament_faction_stats(request,
+                             tournament_id = None):
+    tournament = get_tournament(tournament_id)
+    return faction_stats(request, tournament=tournament)
+
+def league_faction_stats(request,
+                         league_id = None):
+    league = get_league(league_id)
+    return faction_stats(request, league=league)
+
+def get_tournament(tournament_id = None):
+    tournaments = None
+    tournament = None
+    if (tournament_id not in EMPTY_VALUES):
+        tournaments = Tournament.objects.filter(id=tournament_id)
+    if (tournaments not in EMPTY_VALUES and len(tournaments) == 1):
+        tournament = tournaments.first()
+    if (tournament in EMPTY_VALUES):
+        tournament, _ = Tournament.get_default()
+    return tournament
+
+def get_league(league_id = None):
+    leagues = None
+    league = None
+    if (league_id not in EMPTY_VALUES):
+        leagues = League.objects.filter(id=league_id)
+    if (leagues not in EMPTY_VALUES and len(leagues) == 1):
+        league = leagues.first()
+    if (league in EMPTY_VALUES):
+        league, _ = League.get_default()
+    return league
+
+def get_title(tournament = None,
+              league = None,
+              default = _("All games")):
+    if (tournament not in EMPTY_VALUES):
+        title = tournament.name
+    elif (league not in EMPTY_VALUES):
+        title = league.name
+    else:
+        title = default
+    return title
+
+def get_menu_by_pagination(league = None,
+                           tournament = None):
     if (league in EMPTY_VALUES and
         tournament not in EMPTY_VALUES):
         league = tournament.league
@@ -72,54 +183,8 @@ def leaderboard(request,
     elif (season_paginator.count >= 1):
         season_range = season_paginator.get_elided_page_range(1)
     
-    return ElidedListView.as_view(model=Player,
-                                  queryset=players,
-                                  template_name='league/leaderboard.html',
-                                  paginate_by=number_per_page,
-                                  title=title,
-                                  ordering=ordering,
-                                  extra_context=dict(min_games=min_games,
-                                                     league=league,
-                                                     seasons=seasons,
-                                                     season_page=season_page,
-                                                     season_range=season_range,
-                                                     season_paginator=season_paginator)
-                                  )(request)
-
-def league_leaderboard(request,
-                       league_id = None,
-                       ordering = '-relative_score',
-                       number_per_page = 15):
-    leagues = None
-    league = None
-    if (league_id not in EMPTY_VALUES):
-        leagues = League.objects.filter(id=league_id)
-    if (leagues not in EMPTY_VALUES and len(leagues) == 1):
-        league = leagues.first()
-    if (league in EMPTY_VALUES):
-        league, _ = League.get_default()
-    return leaderboard(request, league=league,
-                       ordering=ordering,
-                       number_per_page=number_per_page)
-
-def tournament_leaderboard(request,
-                           tournament_id = None,
-                           ordering = '-relative_score',
-                           number_per_page = 15):
-    tournaments = None
-    tournament = None
-    if (tournament_id not in EMPTY_VALUES):
-        tournaments = Tournament.objects.filter(id=tournament_id)
-    if (tournaments not in EMPTY_VALUES and len(tournaments) == 1):
-        tournament = tournaments.first()
-    if (tournament in EMPTY_VALUES):
-        tournament, _ = Tournament.get_default()
-    return leaderboard(request, tournament=tournament,
-                       ordering=ordering,
-                       number_per_page=number_per_page)
-
-def global_leaderboard(request,
-                       ordering = '-relative_score',
-                       number_per_page = 15):
-    return leaderboard(request, ordering=ordering,
-                       number_per_page=number_per_page)
+    return dict(league=league,
+                seasons=seasons,
+                season_page=season_page,
+                season_range=season_range,
+                season_paginator=season_paginator)
