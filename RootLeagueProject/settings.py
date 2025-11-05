f9ee6ce0ceecd5cc59d2ea56fded0f348a19afa9
Pliskin <nabil14@gmail.com>
2025-11-05 21:44:42 +0100

    API throttle settings

--- RootLeagueProject/settings.py
+++ RootLeagueProject/settings.py
@@ -243,7 +243,17 @@ IMPORT_EXPORT_SKIP_ADMIN_ACTION_EXPORT_UI = True
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': 50,
-    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
+    
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
+
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle'
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '100/day',
+        'user': '1000/day'
+    }
 }
 
 
