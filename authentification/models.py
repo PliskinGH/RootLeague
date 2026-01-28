0d420cc43f30cbaaa205c86ec3db374ae01a5ee5
Pliskin <nabil14@gmail.com>
2026-01-28 22:16:00 +0100

    fix validation of ig name, ig id and discord username

--- authentification/models.py
+++ authentification/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 from django.db.models.functions import Lower
 from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.validators import UnicodeUsernameValidator
 from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
@@ -20,6 +21,21 @@ class Player(AbstractUser):
     """
     Model for players registered in the league database.
     """
+
+    discord_validator = UnicodeUsernameValidator(
+        regex = r"^[a-z0-9._]+\Z",
+        message = _(
+        "Enter a valid Discord username. This value may contain only unaccented lowercase letters, "
+        "numbers, and ./_ characters."
+    ))
+
+    ign_validator = UnicodeUsernameValidator(
+        regex = r"^[\w.@-]+\Z",
+        message = _(
+        "Enter a valid username. This value may contain only unaccented lowercase a-z "
+        "and uppercase A-Z letters, numbers, and @/./-/_ characters."
+    ))
+
     first_name = None
     last_name = None
     email = models.EmailField(_('email address'), unique=True,
@@ -27,11 +43,14 @@ class Player(AbstractUser):
     discord_name = models.CharField(max_length=200,
                                     blank=True, null=True, default=None,
                                     unique=True,
-                                    verbose_name=_("discord username"))
-    in_game_name = models.CharField(max_length=200, blank=True,
-                                    verbose_name=_("in-game Username"))
-    in_game_id = models.IntegerField(blank=True, null=True,
-                                     verbose_name=_("in-game ID"))
+                                    verbose_name=_("Discord username"),
+                                    validators=[discord_validator],)
+    in_game_name = models.CharField(max_length=200,
+                                    blank=False,
+                                    verbose_name=_("in-game username"),
+                                    validators=[ign_validator],)
+    in_game_id = models.IntegerField(verbose_name=_("in-game ID"),
+                                     blank=False, null=True)
     roles = models.ManyToManyField(Role, related_name='players',
                                    blank=True,
                                    verbose_name=_('roles'))
@@ -43,7 +62,8 @@ class Player(AbstractUser):
             models.UniqueConstraint(
                 Lower('in_game_name'),
                 "in_game_id",
-                name='unique_in_game_id'
+                name='unique_in_game_id',
+                violation_error_message=_("A user with that ign+igid already exists."),
             ),
         ]
     
